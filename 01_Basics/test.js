a09ab00c207522a40711f15c9d647656bbc9fb1f
hardikmishra0910 <142745304+hardikmishra0910@users.noreply.github.com>
2025-07-13 21:08:48 +0000

    update it

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,44 @@
-console.log("happy")
+// console.log("happy")
+// let n = new Number(100)
+// console.log(typeof n);// object data type
+
+// let date = new Date();
+// console.log(date);
+
+// let myTimeStamp = Date.now();
+// console.log(date.getTime());
+
+
+
+//  function addTwoNumbers(number1, number2){
+//      console.log(number1 + number2);
+// }
+
+
+
+// console.log(addTwoNumbers(3, 5)) // it will give first 8 then give us value as undefined
+// const result = addTwoNumbers(5,3)
+// console.log(result)
+
+
+const month = "april"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+        
+}
